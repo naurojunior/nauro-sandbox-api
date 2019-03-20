dfab3162363860006238cce1c2c50be1c625b15e
Nauro <nauro@outlook.com>
2019-03-20 02:45:24 -0300

    Adding express

--- server.js
+++ server.js
@@ -3,21 +3,14 @@ const fs = require("fs"),
       YoutubePlaylistAPI = require("./YoutubePlaylistAPI.js"),
       request = require("request");
 
-const port = 4000
-const ip = '0.0.0.0'
+var express = require('express');
+var app = express();
 
 
-const server = http.createServer(async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-   let videosList = await new YoutubePlaylistAPI().getVideos();
-   res.end(JSON.stringify(videosList));
+app.get('/', async function (req, res) {
+  let videosList = await new YoutubePlaylistAPI().getVideos();
+  res.send(videosList);
 });
 
+app.listen(4000, function () {});
 
-server.listen(port, ip, () => {
-  console.log(`Servidor rodando em http://${ip}:${port}`)
-  console.log('Para derrubar o servidor: ctrl + c');
-});
